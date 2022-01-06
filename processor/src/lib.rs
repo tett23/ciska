68f59659c9cd2e1e038bcb6130fed83661b657bd
tett23 <tett23@gmail.com>
2022-01-07 05:12:59 +0900

    wip

--- processor/src/lib.rs
+++ processor/src/lib.rs
@@ -4,7 +4,10 @@ mod parser;
 
 use parser::{Parser, ParserProcessor};
 
-pub enum Processor<N> {
+pub enum Processor<N>
+where
+    N: 'static,
+{
     None,
     ParserProcessor(ParserProcessor<N>),
 }
@@ -14,8 +17,11 @@ impl<N> Processor<N> {
         Processor::None
     }
 
-    pub fn parser(&self, f: Box<dyn Parser<N>>) -> Processor<N> {
-        Processor::ParserProcessor(ParserProcessor::new(f))
+    pub fn parser<F>(&self, f: F) -> Processor<N>
+    where
+        F: Fn(&str) -> Result<Box<N>, String> + 'static,
+    {
+        Processor::ParserProcessor(ParserProcessor::new(into_parser(f)))
     }
 
     pub fn parse(&self, text: &str) -> Result<Box<N>, String> {
@@ -55,16 +61,9 @@ where
     F: Fn(&str) -> Result<Box<R>, String> + 'static,
     R: 'static,
 {
-    let c = move |a: String| {
-        let b = match f(&a) {
-            Ok(v) => Result::Ok(v),
-            Err(e) => Err(e),
-        };
-
-        b
-    };
-
-    Box::new(P2 { f: c })
+    Box::new(P2 {
+        f: move |text: String| f(&text),
+    })
 }
 
 #[cfg(test)]
@@ -74,22 +73,22 @@ mod tests {
     #[test]
     fn it_works() {
         let actual = Processor::<i32>::new()
-            .parser(into_parser(|_text: &str| Ok(Box::new(1))))
+            .parser(|_text: &str| Ok(Box::new(1)))
             .parse("test");
         assert_eq!(Ok(Box::new(1)), actual);
 
         let actual = Processor::<String>::new()
-            .parser(into_parser(|text: &str| Ok(Box::new(text.to_string()))))
+            .parser(|text: &str| Ok(Box::new(text.to_string())))
             .parse("test");
         assert_eq!(Ok(Box::new("test".to_string())), actual);
 
         let actual = Processor::<Vec<i32>>::new()
-            .parser(into_parser(|_: &str| Ok(Box::new(vec![1]))))
+            .parser(|_: &str| Ok(Box::new(vec![1])))
             .parse("test");
         assert_eq!(Ok(Box::new(vec![1])), actual);
 
-        let actual = Processor::<i32>::new()
-            .parser(into_parser(|_: &str| Err("error".to_string())))
+        let actual = Processor::<String>::new()
+            .parser(|_: &str| -> Result<Box<String>, String> { Err("error".to_string()) })
             .parse("test");
         assert_eq!(Err("error".to_string()), actual);
     }
