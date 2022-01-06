29ef473668a1c581cea4604146fb2234378fa440
tett23 <tett23@gmail.com>
2022-01-07 05:42:45 +0900

    Implement CLI command

--- prose-parser/src/lib.rs
+++ prose-parser/src/lib.rs
@@ -3,6 +3,7 @@ extern crate pest;
 extern crate pest_derive;
 
 mod parser;
+pub use parser::Node;
 
 pub fn parse(document: &str) -> Result<parser::Node, String> {
     parser::parse(document)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate processor;
+extern crate prose_parser;
+
+use processor::Processor;
+use prose_parser::Node;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", parse("").unwrap().to_json().unwrap());
+}
+
+fn parse(text: &str) -> Result<Node, String> {
+    Processor::<Node>::new()
+        .parser(prose_parser::parse)
+        .parse(text)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        dbg!(parse("").unwrap());
+    }
 }
