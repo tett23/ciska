a257f79c607c22a1f6cc2fd876a36296c5b623c3
tett23 <tett23@gmail.com>
2022-01-07 05:21:39 +0900

    Remove unnecessary Box

--- processor/src/lib.rs
+++ processor/src/lib.rs
@@ -1,8 +1,6 @@
-// #![feature(inherent_associated_types)]
-
 mod parser;
 
-use parser::{Parser, ParserProcessor};
+use parser::ParserProcessor;
 
 pub enum Processor<N>
 where
@@ -19,53 +17,17 @@ impl<N> Processor<N> {
 
     pub fn parser<F>(&self, f: F) -> Processor<N>
     where
-        F: Fn(&str) -> Result<Box<N>, String> + 'static,
+        F: Fn(&str) -> Result<N, String> + 'static,
     {
         Processor::ParserProcessor(ParserProcessor::new(f.into()))
     }
 
-    pub fn parse(&self, text: &str) -> Result<Box<N>, String> {
+    pub fn parse(&self, text: &str) -> Result<N, String> {
         match self {
             Processor::None => Err("".to_string()),
             Processor::ParserProcessor(processor) => processor.parse(text),
         }
     }
-}
-
-impl<F, R> From<F> for Box<dyn Parser<R>>
-where
-    F: Fn(&str) -> Result<Box<R>, String> + 'static,
-    R: 'static,
-{
-    fn from(f: F) -> Self {
-        into_parser(f)
-    }
-}
-
-struct P2<F, R>
-where
-    F: Fn(String) -> Result<Box<R>, String>,
-{
-    f: F,
-}
-
-impl<F, R> Parser<R> for P2<F, R>
-where
-    F: Fn(String) -> Result<Box<R>, String>,
-{
-    fn parse(&self, text: String) -> Result<Box<R>, String> {
-        (self.f)(text)
-    }
-}
-
-fn into_parser<F, R>(f: F) -> Box<dyn Parser<R>>
-where
-    F: Fn(&str) -> Result<Box<R>, String> + 'static,
-    R: 'static,
-{
-    Box::new(P2 {
-        f: move |text: String| f(&text),
-    })
 }
 
 #[cfg(test)]
@@ -75,22 +37,22 @@ mod tests {
     #[test]
     fn it_works() {
         let actual = Processor::<i32>::new()
-            .parser(|_text: &str| Ok(Box::new(1)))
+            .parser(|_text: &str| Ok(1))
             .parse("test");
-        assert_eq!(Ok(Box::new(1)), actual);
+        assert_eq!(Ok(1), actual);
 
         let actual = Processor::<String>::new()
-            .parser(|text: &str| Ok(Box::new(text.to_string())))
+            .parser(|text: &str| Ok(text.to_string()))
             .parse("test");
-        assert_eq!(Ok(Box::new("test".to_string())), actual);
+        assert_eq!(Ok("test".to_string()), actual);
 
         let actual = Processor::<Vec<i32>>::new()
-            .parser(|_: &str| Ok(Box::new(vec![1])))
+            .parser(|_: &str| Ok(vec![1]))
             .parse("test");
-        assert_eq!(Ok(Box::new(vec![1])), actual);
+        assert_eq!(Ok(vec![1]), actual);
 
         let actual = Processor::<String>::new()
-            .parser(|_: &str| -> Result<Box<String>, String> { Err("error".to_string()) })
+            .parser(|_: &str| Err("error".to_string()))
             .parse("test");
         assert_eq!(Err("error".to_string()), actual);
     }
--- processor/src/parser.rs
+++ processor/src/parser.rs
@@ -1,55 +1,55 @@
-// use std::rc::Rc;
-
 pub trait Parser<R> {
-    fn parse(&self, text: String) -> Result<Box<R>, String>;
+    fn parse(&self, text: String) -> Result<R, String>;
+}
+
+impl<F, R> From<F> for Box<dyn Parser<R>>
+where
+    F: Fn(&str) -> Result<R, String> + 'static,
+    R: 'static,
+{
+    fn from(f: F) -> Self {
+        into_parser(f)
+    }
 }
 
 struct P2<F, R>
 where
-    F: Fn(String) -> Result<Box<R>, String>,
+    F: Fn(String) -> Result<R, String>,
 {
     f: F,
 }
 
 impl<F, R> Parser<R> for P2<F, R>
 where
-    F: Fn(String) -> Result<Box<R>, String>,
+    F: Fn(String) -> Result<R, String>,
 {
-    fn parse(&self, text: String) -> Result<Box<R>, String> {
+    fn parse(&self, text: String) -> Result<R, String> {
         (self.f)(text)
     }
 }
 
-pub struct ParserProcessor<R>
+fn into_parser<F, R>(f: F) -> Box<dyn Parser<R>>
 where
-// R: 'a,
+    F: Fn(&str) -> Result<R, String> + 'static,
+    R: 'static,
 {
+    Box::new(P2 {
+        f: move |text: String| f(&text),
+    })
+}
+
+pub struct ParserProcessor<R> {
     parser: Box<dyn Parser<R>>,
 }
 
-impl<R> ParserProcessor<R>
-where
-// R: 'a,
-{
+impl<R> ParserProcessor<R> {
     pub fn new(f: Box<dyn Parser<R>>) -> ParserProcessor<R> {
-        // let a: Box<dyn Parser<R> + 'a> = Box::new(P2 { f });
-        // let a: Box<dyn Parser<R>> = Box::new(P2 { f });
-
         ParserProcessor { parser: f }
     }
 
-    pub fn parse(&self, text: &str) -> Result<Box<R>, String> {
+    pub fn parse(&self, text: &str) -> Result<R, String> {
         self.parser.parse(text.to_string())
     }
-
-    // fn new(f: impl Fn(&str) -> R + 'a) -> &dyn P2<R> {}
-
-    // pub fn parser<F, R>(&self, parser: Box<F>) -> Processor<F, R>
-    // where
-    //     F: Fn(&str) -> R,
-    // {
-    //     let f = Processor::new.clone();
-    // }
 }
 
 #[cfg(test)]
