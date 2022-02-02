ba056f2ae158a664980a92ae655c59bfd0fabe6c
tett23 <tett23@gmail.com>
2022-02-02 19:16:18 +0900

    Add empty package

---
+++ packages/gql/build.mjs
@@ -0,0 +1,30 @@
+import { build } from 'esbuild';
+import glob from 'glob';
+import process from 'process';
+import fs from 'fs/promises';
+
+const entryPoints = glob.sync('./src/**/*.ts');
+const watch = process.argv.some((item) => item === '--watch');
+
+await build({
+  outdir: `lib/cjs`,
+  entryPoints: entryPoints,
+  entryNames: '[dir]/[name]',
+  platform: 'node',
+  format: 'cjs',
+  watch,
+  sourcemap: true,
+});
+
+await build({
+  outdir: `lib/esm`,
+  entryPoints: entryPoints,
+  entryNames: '[dir]/[name]',
+  platform: 'node',
+  format: 'esm',
+  watch,
+  sourcemap: true,
+});
+
+await fs.copyFile('./src/index.html', './lib/cjs/index.html');
+await fs.copyFile('./src/index.html', './lib/esm/index.html');
