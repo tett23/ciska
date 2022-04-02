ad525e77a23fd6d811c04794997a3b91903fa82d
tett23 <tett23@gmail.com>
2022-04-02 14:19:01 +0900

    Update build settings

--- packages/menu-frontend/src/index.ts
+++ packages/menu-frontend/src/index.ts
@@ -1,3 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import { createElement } from 'react';
+import { App } from './components';
+
 (() => {
-  alert('test');
+  const el = document.querySelector('#app');
+  if (el == null) {
+    return;
+  }
+  const root = createRoot(el);
+
+  root.render(createElement(App));
 })();
